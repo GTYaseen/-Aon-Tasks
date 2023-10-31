66d2a8d929bbe77f32dd535426dff42807f387c8
GTYaseen <Xxyaseent11xX14@gmail.com>
2023-10-31 03:59:51 +0300

    Task

---
+++ app.js
@@ -0,0 +1,37 @@
+let helper = require("./helper");
+NewUser = {
+  id: 11,
+  name: "Yaseen Taha",
+  username: "GTYaseen",
+  email: "steamgtyaseent11@gmail.com",
+  address: {
+    street: "Douglas Extension",
+    suite: "Suite 847",
+    city: "Baghdad",
+    zipcode: "10001",
+    geo: {
+      lat: "-68.6102",
+      lng: "-47.0653",
+    },
+  },
+  phone: "07714230015",
+  website: "no",
+  company: {
+    name: "Romaguera-Jacobson",
+    catchPhrase: "Face to face bifurcated interface",
+    bs: "e-enable strategic applications",
+  },
+};
+let list = helper.getAllUser();
+//console.log(list);
+
+console.log(helper.getFirstUser(list));
+console.log(helper.getLast(list));
+console.log(helper.getUserByID(list, 9));
+console.log(helper.getUserByCompanyName(list, "Deckow-Crist"));
+console.log(helper.getUsersByCity(list, "McKenziehaven"));
+console.log(helper.getStreet(list, 2));
+console.log(helper.addNewUser(list, NewUser));
+console.log(helper.updateUser(list, 2, "steamgtyaseent11@gmail.com"));
+//for update user (list, userID, newEmail)
+console.log(helper.deleteUserById(list, 1));
---
+++ helper.js
@@ -0,0 +1,72 @@
+fs = require("fs");
+const getAllUser = () => {
+  //TODO: Read file and return users here
+  try {
+    let resp = fs.readFileSync("users.json", "utf-8");
+    let list = JSON.parse(resp);
+    return list;
+  } catch (err) {
+    console.log(err);
+  }
+  return list;
+};
+const getFirstUser = (list) => {
+  //TODO: get first user of list
+  let firstUser = list[0];
+  return firstUser;
+};
+const getLast = (list) => {
+  //TODO: get last user of list
+  let lastUser = list[list.length - 1];
+  return lastUser;
+};
+const getUserByID = (list, userID) => {
+  //TODO: get user object by id
+  let user = list.find((user) => user.id === userID);
+  return user;
+};
+const getUserByCompanyName = (list, companyName) => {
+  //TODO: get user object by company name
+  let user = list.find((user) => user.company.name === companyName);
+  return user;
+};
+const getUsersByCity = (list, city) => {
+  //TODO: get users that live in this city
+  let user = list.filter((user) => user.address.city === city);
+  return user;
+};
+const getStreet = (list, userID) => {
+  //TODO: get street name by userID
+  let UserS = list.find((user) => user.id === userID);
+  return UserS.address.street;
+};
+const addNewUser = (list, userObject) => {
+  //TODO: add new user to the list
+  let newList = list;
+  newList.push(userObject);
+  return newList;
+};
+const updateUser = (list, userID, newEmail) => {
+  //TODO: update email when id === userID
+  let user = list.find((user) => user.id === userID);
+  if (user.id === userID) user.email = newEmail;
+  return user;
+};
+const deleteUserById = (list, userID) => {
+  //TODO: remove user when id === userID
+  let user = list.find((user) => user.id === userID);
+  if (user.id === userID) list = list.filter((user) => user.id !== userID);
+  return list;
+};
+module.exports = {
+  getAllUser,
+  getFirstUser,
+  getLast,
+  getUserByID,
+  getUserByCompanyName,
+  getUsersByCity,
+  getStreet,
+  addNewUser,
+  updateUser,
+  deleteUserById,
+};
